6f47779709ec4d4b38bd8fde78e91654351fb869
CoDeViNi <viniciusmagalhaes2y@gmail.com>
2025-06-13 09:42:48 -0300

    Update contasapagar.py

--- contasapagar.py
+++ contasapagar.py
@@ -1092,7 +1092,7 @@ elif page == "Contas a Pagar":
                 else:
                     st.error("Erro ao salvar alterações.")
 
-    
+    with st.expander("🗑️ Remover Registro"):
         if not df_display.empty:
             idx_rem = st.number_input(
                 "Índice da linha para remover:",
